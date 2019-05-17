59ea5e96471cc36139ec44a1263641022da8c745
Armando <armandoa7x@gmail.com>
2019-05-17 13:08:17 -0500

    Actualizamos el proyecto desde Git Bash, capitulo 14

--- src/test/java/pages/PageLogin.java
+++ src/test/java/pages/PageLogin.java
@@ -1,9 +1,12 @@
 package pages;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class PageLogin {
 
@@ -11,6 +14,7 @@ public class PageLogin {
 	private By userField;
 	private By passField;
 	private By loginBtn;
+	private By fields;
 	
 	public PageLogin(WebDriver driver) {
 		this.driver = driver;
@@ -18,6 +22,7 @@ public class PageLogin {
 		userField = By.name("userName");
 		passField = By.name("password");
 		loginBtn = By.name("login");
+		fields = By.tagName("input");
 	}
 	
 	//Trabajo de Login
@@ -26,9 +31,27 @@ public class PageLogin {
 		driver.findElement(userField).sendKeys(user);
 		driver.findElement(passField).sendKeys(pass);
 		driver.findElement(loginBtn).click();
-		//Helpers helper = new Helpers();
-		//helper.sleepSeconds(4);
+		/*File myScreenShotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(myScreenShotFile, new File("LOGIN " + System.currentTimeMillis()+".png"));
+			} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			} */
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	public void fields_login(String user, String pass) {
+		List<WebElement> loginFields = driver.findElements(fields);
+		loginFields.get(1).sendKeys(user);
+		loginFields.get(2).sendKeys(pass);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	public void verifyFields() {
+		List<WebElement> loginFieldsElements = driver.findElements(fields);
+		System.out.println(loginFieldsElements.size());
+		Assert.assertTrue(loginFieldsElements.size()==5);
 	}
 	
 }
--- src/test/java/tests/Testing.java
+++ src/test/java/tests/Testing.java
@@ -1,9 +1,17 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeMethod;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
@@ -20,7 +28,9 @@ public class Testing {
 		DesiredCapabilities caps = new DesiredCapabilities();
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
+		//driver.manage().window().fullscreen();
+		driver.manage().window().setSize(new Dimension(800, 600));
 		driver.navigate().to("http://newtours.demoaut.com/");
 		Helpers helper = new Helpers();
 		helper.sleepSeconds(4);
@@ -54,8 +64,23 @@ public class Testing {
 		helper.sleepSeconds(2);
 	}
 	
+	@Test
+	public void pruebaCantidadDeCampos() {
+		PageLogin pageLogin = new PageLogin(driver);
+		pageLogin.verifyFields();
+	}
+	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		if(!result.isSuccess()) {
+			File myScreenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			try {
+				FileUtils.copyFile(myScreenshotFile, new File("Error " + System.currentTimeMillis()+".png"));
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
 		driver.close();
 	}
 	
